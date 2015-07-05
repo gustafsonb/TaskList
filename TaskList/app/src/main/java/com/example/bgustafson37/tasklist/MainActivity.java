7db359800c21c228b9bdb626759c1bef3b59f250
Brad Gustafson <gustafsonb37@gmail.com>
2015-07-04 22:48:41 -0500

    WorkingCode

---
+++ TaskList/app/src/main/java/com/example/bgustafson37/tasklist/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.bgustafson37.tasklist;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private ArrayList<String> list;
+    private ArrayAdapter<String> listAdapter;
+    private ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.lvListTask);
+        list = new ArrayList<String>();
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+
+        listView.setAdapter(listAdapter);
+        list.add("Thing One");
+        list.add("Thing Two");
+        list.add("Thing Three");
+
+        setupLongClickListener();
+
+    }
+
+    private  void  setupLongClickListener(){
+        listView.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                        list.remove(position);
+                        listAdapter.notifyDataSetChanged();
+                        return false;
+                    }
+                });
+    }
+
+    public void onAddTask(View v){
+        EditText etNewTask = (EditText)findViewById(R.id.etNewTask);
+        String strNewTask = etNewTask.getText().toString();
+        listAdapter.add(strNewTask);
+        etNewTask.setText("");
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
