a711bea9637adf58bb4ecf101f91bb14e80f5693
Brad Gustafson <gustafsonb37@gmail.com>
2015-07-26 14:02:26 -0500

    Commit 3

--- TaskList/app/src/main/java/com/example/bgustafson37/tasklist/MainActivity.java
+++ TaskList/app/src/main/java/com/example/bgustafson37/tasklist/MainActivity.java
@@ -11,8 +11,11 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import org.apache.commons.io.FileUtils;
 import org.w3c.dom.Text;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -29,14 +32,10 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         listView = (ListView)findViewById(R.id.lvListTask);
-        list = new ArrayList<String>();
+        readFromFile();
         listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
 
         listView.setAdapter(listAdapter);
-        list.add("Thing One");
-        list.add("Thing Two");
-        list.add("Thing Three");
-
         setupLongClickListener();
 
     }
@@ -54,15 +53,35 @@ public class MainActivity extends ActionBarActivity {
                 });
     }
 
-    public void onAddTask(View v){
-        EditText etNewTask = (EditText)findViewById(R.id.etNewTask);
+    public void onAddTask(View v) {
+        EditText etNewTask = (EditText) findViewById(R.id.etNewTask);
         String strNewTask = etNewTask.getText().toString();
-        listAdapter.add(strNewTask);
-        etNewTask.setText("");
+        if (!strNewTask.isEmpty()) {
+            listAdapter.add(strNewTask);
+            etNewTask.setText("");
+            saveToFile();
+        }
     }
 
+    private void saveToFile(){
+        File fileDir = getFilesDir();
+        File todoFile = new File(fileDir, "tasklist.txt");
+        try{
+            FileUtils.writeLines(todoFile,list);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
-
+    private void readFromFile(){
+        File fileDir = getFilesDir();
+        File todoFile = new File(fileDir, "tasklist.txt");
+        try{
+            list = new ArrayList<String>(FileUtils.readLines(todoFile));
+        }catch (IOException e){
+            list = new ArrayList<String>();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
